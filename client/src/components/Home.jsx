b6fcf3435f571a4aa646ea97b6a926874e5fca40
sk6786 <sk6786@g.rit.edu>
2019-03-30 12:59:43 -0400

    fixed images

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import Background from '../images/landing.jpg';
+import Background from '../styles/images/landing.jpg';
 
 
 import {Button} from 'reactstrap';
