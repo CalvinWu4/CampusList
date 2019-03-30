9b81c3335fed9b4a3e6419fa8064cd7993345ff0
sk6786 <sk6786@g.rit.edu>
2019-03-30 17:07:49 -0400

    weird font

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import Home from './components/Home'
 import Register from './components/Register';
-import SignUp from './components/SignUp';
+import Login from './components/Login';
 import Review from './components/Review';
 import HomePage from './components/HomePage';
 
@@ -21,7 +21,7 @@ class App extends Component {
         <BrowserRouter>
             <div>
             <Route path ='/' component={Home} exact/>
-            <Route path ='/signup' component={SignUp}/>
+            <Route path ='/login' component={Login}/>
             <Route path='/register'  component={Register}/>
             <Route path='/review' component = {Review}/>
             <Route path='/homepage' component = {HomePage}/>
--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -2,23 +2,16 @@ import React, {Component} from 'react';
 import style from '../styles/css/styles.css';
 import {Button} from 'reactstrap';
 
-const styles = {
-    header:{
-        fontFamily: "Lato",
-        fontSize:70,
-        color:'white'
-    },
-};
 
 class Home extends React.Component {
     render() {
         return (
             <div>
                 <div className='paperContainer'>
-                    <h1 style={styles.header}>CAMPUS LISTS</h1>
-                    <div style={{display:'inline', marginTop:'15px'}}>
+                    <h1 className='homeHeader'>CAMPUS LISTS</h1>
+                    <div style={{display:'inline', marginTop:'20px'}}>
                     <Button href="/signup" color="secondary" size="lg" className='signUp'>Sign Up</Button>
-                    <Button href="/register" color="primary" size="lg" style={{marginLeft:'15px', lineHeight:'20px'}}>Login</Button>
+                    <Button href="/register" color="primary" size="lg" className='login'>Login</Button>
                     </div>
                 </div>
             </div>
