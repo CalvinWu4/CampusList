fc911c86bcdab14b799d06bf666f63ee404dd165
sk6786 <sk6786@g.rit.edu>
2019-03-30 17:42:47 -0400

    navbar

--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import { Button, Form, FormFeedback, FormGroup, Label, Input, Row, Col, Container} from 'reactstrap';
 import {Link} from 'react-router-dom';
-class HomePage extends React.Component {
+import ReactDOM from 'react-dom';
 
+import navbar from '../components/NavigationBar';
+
+class HomePage extends React.Component {
+    render() {
+        return (
+            <navbar/>
+        )}
 
 }
 
 export default HomePage;
+ReactDOM.render(
+    <HomePage />,
+    document.getElementById('container')
+);
