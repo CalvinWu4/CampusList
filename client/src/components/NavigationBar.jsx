1330a78f42d629f5d8af97eff6fe72e559d502b1
Bailey <bap3215@rit.edu>
2019-03-31 16:34:37 -0400

    nav bar complete

--- client/src/components/NavigationBar.jsx
+++ client/src/components/NavigationBar.jsx
@@ -7,56 +7,65 @@ import {
     Nav,
     NavItem,
     NavLink,
-    UncontrolledDropdown,
+    Dropdown,
     DropdownToggle,
     DropdownMenu,
-    DropdownItem } from 'reactstrap';
+    DropdownItem,
+    Media } from 'reactstrap';
+import style from '../styles/css/styles.css';
+import src from '../styles/images/landing.jpg';
 
-class NavigationBar extends React.Component {
+
+export default class NavigationBar extends React.Component {
     constructor(props) {
-        super(props);
+    super(props);
 
-        this.toggle = this.toggle.bind(this);
-        this.state = {
-            isOpen: false
-        };
-    }
-    toggle() {
-        this.setState({
-            isOpen: !this.state.isOpen
-        });
-    }
+    this.toggle = this.toggle.bind(this);
+    this.onMouseEnter = this.onMouseEnter.bind(this);
+    this.onMouseLeave = this.onMouseLeave.bind(this);
+    this.state = {
+      dropdownOpen: false
+    };
+  }
+
+  toggle() {
+    this.setState(prevState => ({
+      dropdownOpen: !prevState.dropdownOpen
+    }));
+  }
+
+  onMouseEnter() {
+    this.setState({dropdownOpen: true});
+  }
+
+  onMouseLeave() {
+    this.setState({dropdownOpen: false});
+  }
     render() {
         return (
             <div>
-                <Navbar color="light" light expand="md">
-                    <NavbarBrand href="/">reactstrap</NavbarBrand>
+                <Navbar className='navBar' light expand="md">
+                    <img className='navBarLogo' src={require('../styles/images/rit-logo.png')}></img>
+                    <NavbarBrand className='navBarText navBarHeader' href="/">CampusList</NavbarBrand>
                     <NavbarToggler onClick={this.toggle} />
                     <Collapse isOpen={this.state.isOpen} navbar>
                         <Nav className="ml-auto" navbar>
-                            <NavItem>
-                                <NavLink href="/components/">Components</NavLink>
-                            </NavItem>
-                            <NavItem>
-                                <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
-                            </NavItem>
-                            <UncontrolledDropdown nav inNavbar>
-                                <DropdownToggle nav caret>
-                                    Options
+                             <Dropdown className="d-inline-block" onMouseOver={this.onMouseEnter} onMouseLeave={this.onMouseLeave} isOpen={this.state.dropdownOpen} toggle={this.toggle}>
+                                <DropdownToggle className='navBarText' nav caret>
+                                    Profile
                                 </DropdownToggle>
                                 <DropdownMenu right>
                                     <DropdownItem>
-                                        Option 1
+                                        My Services
                                     </DropdownItem>
                                     <DropdownItem>
-                                        Option 2
-                                    </DropdownItem>
-                                    <DropdownItem divider />
-                                    <DropdownItem>
-                                        Reset
+                                        My Appointments
                                     </DropdownItem>
                                 </DropdownMenu>
-                            </UncontrolledDropdown>
+                            </Dropdown>
+                            <NavItem>
+                                <NavLink className='navBarText' href="/components/">Sign Out</NavLink>
+                            </NavItem>
                         </Nav>
                     </Collapse>
                 </Navbar>
@@ -64,4 +73,3 @@ class NavigationBar extends React.Component {
         );
     }
 }
-export default NavigationBar;
