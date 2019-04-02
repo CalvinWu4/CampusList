4a18b5d3059b2abb03946293d719cf8cf8e2fd52
Bailey <bap3215@rit.edu>
2019-04-02 14:11:44 -0400

    adding all text to page

--- client/src/components/Service.jsx
+++ client/src/components/Service.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {Container, Button, Row} from 'reactstrap';
 import {Link} from 'react-router-dom';
 import axios from 'axios';
+import Navbar from './NavigationBar';
 
 
 class Service extends React.Component {
@@ -16,7 +17,27 @@ class Service extends React.Component {
 
     render(){
         return(
-            this.state.listing
+            <div>
+                <Navbar/>
+                <div>
+                    <p>{this.state.listing['category']}</p>
+                    <p>{this.state.listing['title']}</p>
+                    <p>{this.state.listing['price']}</p>
+                    <p>{this.state.listing['rating']}</p>
+                </div>
+                <div>
+                    <div>
+                        <p>Description</p>
+                    </div>
+                    <div>
+                        <p>Reviews</p>
+                    </div>
+                    <div>
+                        <p>Appointments</p>
+                    </div>
+
+                </div>
+            </div>
         );
 
     }
