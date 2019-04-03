bb7bb84d7013ace95bfb9dd442c8701f044411c9
Matthew Seaman <mattsdeveloper@gmail.com>
2019-04-02 23:35:10 -0400

    Add shell appointments page

---
+++ client/src/components/Appointments.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Navbar from './NavigationBar';
+
+export default class Appointments extends React.Component {
+
+    render() {
+	return (
+	    <div><b>Appointments</b></div>
+    )}
+
+}
