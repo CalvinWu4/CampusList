d1b3f0fcac4b43e5f46f1578846db379c7bfa111
Matthew Seaman <mattsdeveloper@gmail.com>
2019-04-03 17:48:10 -0400

    Add report icon

--- client/src/components/AppointmentEvent.jsx
+++ client/src/components/AppointmentEvent.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faExclamationTriangle } from '@fortawesome/free-solid-svg-icons'
+
+library.add( faExclamationTriangle );
 
 export default class AppointmentEvent extends React.Component {
 
     render() {
 	return (
 	    <div style={{ display: 'inline' }}>
-	    	<p>{this.props.event.title}</p>
+	    	<span style={{ marginRight: '1em' }}>{this.props.event.title}</span>
+	    	<FontAwesomeIcon icon="exclamation-triangle" />
 	    </div>
 	)
     }
