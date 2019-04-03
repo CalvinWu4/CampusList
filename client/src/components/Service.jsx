71aac88295ad7029dcbeed22e927600a68727f75
Bailey <bap3215@rit.edu>
2019-04-03 11:14:56 -0400

    page in progress

--- client/src/components/Service.jsx
+++ client/src/components/Service.jsx
@@ -1,46 +1,94 @@
 import React from 'react';
-import {Container, Button, Row} from 'reactstrap';
-import {Link} from 'react-router-dom';
-import axios from 'axios';
+import { TabContent, TabPane, Nav, NavItem, NavLink, Card, Button, CardTitle, CardText, Row, Col } from 'reactstrap';
+import classnames from 'classnames';
 import Navbar from './NavigationBar';
 
+export default class Example extends React.Component {
+  constructor(props) {
+    super(props);
 
-class Service extends React.Component {
-    state={
-        listing:""
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      activeTab: '1',
+      listing:""
     };
+  }
+
+
     componentDidMount() {
         if (this.props.location.state) {
             this.setState({listing:this.props.location.state});
         }
     }
 
-    render(){
-        return(
-            <div>
-                <Navbar/>
-                <div>
-                    <p>{this.state.listing['category']}</p>
-                    <p>{this.state.listing['title']}</p>
-                    <p>{this.state.listing['price']}</p>
-                    <p>{this.state.listing['rating']}</p>
-                </div>
-                <div>
-                    <div>
-                        <p>Description</p>
-                    </div>
-                    <div>
-                        <p>Reviews</p>
-                    </div>
-                    <div>
-                        <p>Appointments</p>
-                    </div>
-
-                </div>
-            </div>
-        );
-
+  toggle(tab) {
+    if (this.state.activeTab !== tab) {
+      this.setState({
+        activeTab: tab
+      });
     }
+  }
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <div style={{display:'flex', flexPosition:'row'}}>
+            <img src={require("../styles/images/"+this.state.listing['picture'])}/>
+            <p>{this.state.listing['category']}</p>
+            <p>{this.state.listing['title']}</p>
+            <p>{this.state.listing['price']}</p>
+            <p>{this.state.listing['rating']}</p>
+        </div>
+        <Nav tabs style={{width:'50%', marginRight:'auto', marginLeft:'auto'}}>
+          <NavItem>
+            <NavLink
+              className={classnames({ active: this.state.activeTab === '1' })}
+              onClick={() => { this.toggle('1'); }}
+            >
+              Description
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink
+              className={classnames({ active: this.state.activeTab === '2' })}
+              onClick={() => { this.toggle('2'); }}
+            >
+              Reviews
+            </NavLink>
+          </NavItem>
+           <NavItem>
+            <NavLink
+              className={classnames({ active: this.state.activeTab === '3' })}
+              onClick={() => { this.toggle('3'); }}
+            >
+              Appointments
+            </NavLink>
+          </NavItem>
+        </Nav>
+        <TabContent activeTab={this.state.activeTab} style={{width:'50%', height:'300px', marginRight:'auto', marginLeft:'auto', border:'solid'}}>
+          <TabPane tabId="1">
+            <Row>
+              <Col sm="12">
+                <p>{this.state.listing['description']}</p>
+              </Col>
+            </Row>
+          </TabPane>
+           <TabPane tabId="2">
+            <Row>
+              <Col sm="12">
+                <h4>Tab 2 Contents</h4>
+              </Col>
+            </Row>
+          </TabPane>
+           <TabPane tabId="3">
+            <Row>
+              <Col sm="12">
+                <h4>Tab 3 Contents</h4>
+              </Col>
+            </Row>
+          </TabPane>
+        </TabContent>
+      </div>
+    );
+  }
 }
-
-export default Service;
