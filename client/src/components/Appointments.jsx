a9d194a2160da579cdc9b5173cc2dada08c59409
Matthew Seaman <mattsdeveloper@gmail.com>
2019-04-03 01:06:33 -0400

    Add calendar

--- client/src/components/Appointments.jsx
+++ client/src/components/Appointments.jsx
@@ -2,12 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Navbar from './NavigationBar';
 
+import moment from 'moment';
+import BigCalendar from 'react-big-calendar';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+const localizer = BigCalendar.momentLocalizer(moment)
+
 export default class Appointments extends React.Component {
 
     render() {
 	return (
 	    <div>
 	        <Navbar/>
+	    	<BigCalendar
+	    	    localizer={localizer}
+	    	    events={[]}
+	    	    view="month"
+	    	    defaultView="month"
+	    	    views={["month"]}
+	    	    style={{ height: "90vh" }}
+	    	/>
 	    </div>
     )}
 
