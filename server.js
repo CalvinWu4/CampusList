eb3ba65c6b03358d015793e0aa4e7e1ae28f75dc
Matthew Seaman <mattsdeveloper@gmail.com>
2019-04-23 12:47:03 -0400

    Serve status React files from Node server

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express= require('express');
 
+const path = require('path');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app=express();
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 //Bodyparser middleware
 app.use(bodyParser.json());
